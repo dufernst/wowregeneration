49ae138b4b20056d940351bc85efbc3a3cf8a1ca
dufernst <dufernst@epicduf.nl>
2014-09-17 16:23:26 +0200

    Add installer icon.

--- Source/WoWRegeneration/WoWRegeneration/UI/ProgressBar.Designer.cs
+++ Source/WoWRegeneration/WoWRegeneration/UI/ProgressBar.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProgressBar));
             this.playButton = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.playButton)).BeginInit();
             this.SuspendLayout();
@@ -50,6 +51,7 @@
             this.ClientSize = new System.Drawing.Size(799, 118);
             this.Controls.Add(this.playButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "ProgressBar";
             this.Text = "DutchWoW Installer";
